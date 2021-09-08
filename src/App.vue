32afaf07ca745497d63ed794266b5d42c4462383
paulking321 <oluwadamilare321@gmail.com>
2021-09-08 10:11:24 +0100

    My Calculator-app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <calculator />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import calculator from './components/calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    calculator
   }
 }
 </script>
@@ -23,4 +22,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+  margin: 0;
+  background: #eee;
+}
+
 </style>
